c0929cc74d0361c1f5b35d8bb3012b1be699b52a
nck272 <nck272.dev@gmail.com>
2025-11-02 18:28:39 +0700

    add: basic implementation for terminal command

---
+++ source/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"strings"
+)
+
+const PATH_POKEDEX string = "../data/pokemon.json"
+
+type pokemon struct {
+	Id          int               `json: "id"`
+	Name        map[string]string `json:"name"`
+	Type        []string          `json:"type"`
+	Description string            `json:"description"`
+}
+
+func GetPokedex() *[]pokemon {
+	f, err := os.ReadFile(PATH_POKEDEX)
+	if err != nil {
+		log.Fatalf("ERROR: Could not read data from %v: %v", PATH_POKEDEX, err)
+	}
+
+	var pokemons []pokemon
+	json.Unmarshal(f, &pokemons)
+
+	return &pokemons
+}
+
+func GetDocTokens(pokemons []pokemon) *doc_tokens {
+	doc_tokens := doc_tokens{}
+	for _, pokemon := range pokemons {
+		name := pokemon.Name["english"]
+		tokens := Tokenize(pokemon.Description)
+		doc_tokens[name] = tokens
+	}
+	return &doc_tokens
+}
+
+func main() {
+	if len(os.Args) <= 1 {
+		log.Fatalf("ERROR: missing arguements, please input the text you want to search!")
+	}
+	input_str := strings.Join(os.Args[1:], " ")
+
+	// Get tf-idf of pokemon descriptions
+	pokemons := GetPokedex()
+	doc_tokens := GetDocTokens(*pokemons)
+
+	// Search and Sort the result descending by cosine_similarity
+	search_service := GetSearchService(*doc_tokens)
+	search_results := search_service.Search(input_str, true)
+
+	// Return result which is a list of description that match with input text
+	log.Println("--- BEST MATCHES: ")
+	for i, result := range search_results {
+		log.Printf("[%3d] %v", i, result)
+	}
+}
---
+++ source/search_service.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"math"
+	"sort"
+	"strings"
+)
+
+const IS_DEBUG = true
+
+type search_service struct {
+	DocFreq        metric
+	InverseDocFreq metric
+	TermIDF        map[string]metric
+}
+
+type match struct {
+	Result string
+	Score  float64
+}
+
+type doc_tokens map[string][]string
+type metric map[string]float64
+
+func GetSearchService(_doc_tokens doc_tokens) *search_service {
+	service := search_service{}
+	service.InverseDocFreq = *CalcIDF(_doc_tokens)
+	service.TermIDF = *GetTermFreqIDF(_doc_tokens, service.InverseDocFreq)
+	return &service
+}
+
+func (sv search_service) Search(input_str string, is_asc bool) []string {
+	input_tokens := Tokenize(input_str)
+	input_tfidf := CalcTF_IDF(input_tokens, sv.InverseDocFreq)
+	matches := sv.FindMatches(*input_tfidf)
+	sorted_matches := SortMatches(matches, is_asc)
+	return sorted_matches
+}
+
+func (sv search_service) FindMatches(input_tfidf metric) []match {
+	matches := []match{}
+	for name, tf_idf := range sv.TermIDF {
+		cosine_similarity := CalcCosineSimilarity(input_tfidf, tf_idf)
+		if cosine_similarity > 0 {
+			matches = append(matches, match{
+				Result: name,
+				Score:  cosine_similarity,
+			})
+		}
+	}
+	return matches
+}
+
+func SortMatches(matches []match, is_asc bool) []string {
+	results := make([]string, 0, len(matches))
+	for i := range matches {
+		results = append(results, matches[i].Result)
+	}
+	sort.SliceStable(results, func(i, j int) bool {
+		if is_asc {
+			return matches[i].Score > matches[j].Score
+		} else {
+			return matches[i].Score < matches[j].Score
+		}
+	})
+	return results
+}
+
+func GetTermFreqIDF(_doc_tokens doc_tokens, idf metric) *map[string]metric {
+	tf_idf := map[string]metric{}
+	for doc, tokens := range _doc_tokens {
+		tf_idf[doc] = *CalcTF_IDF(tokens, idf)
+	}
+	return &tf_idf
+}
+
+func CalcTF_IDF(tokens []string, idf metric) *metric {
+	tf := metric{}
+	for _, token := range tokens {
+		tf[token] = tf[token] + 1
+	}
+
+	tf_idf := metric{}
+	for key := range tf {
+		tf[key] = tf[key] / float64(len(tokens))
+		tf_idf[key] = tf[key] * idf[key]
+	}
+	return &tf_idf
+}
+
+func CalcIDF(_docs_tokens doc_tokens) *metric {
+	tokens := []string{}
+	for _, _tokens := range _docs_tokens {
+		for _, token := range _tokens {
+			tokens = append(tokens, token)
+		}
+	}
+
+	df := metric{}
+	for _, token := range tokens {
+		df[token] = df[token] + 1
+	}
+
+	idf := metric{}
+	for _, token := range tokens {
+		val := float64(len(_docs_tokens)) / float64(df[token]+1)
+		idf[token] = math.Log(val)
+	}
+	return &idf
+}
+
+func Standardlize(s string) string {
+	s = strings.ToLower(s)
+	s = strings.TrimSpace(s)
+	return s
+}
+
+func Tokenize(s string) []string {
+	excludes := []string{",", "."}
+	for _, c := range excludes {
+		s = strings.ReplaceAll(s, c, "")
+	}
+	tokens := strings.Split(s, " ")
+	for key := range tokens {
+		tokens[key] = Standardlize(tokens[key])
+	}
+	return tokens
+}
+func CalcCosineSimilarity(vec1 map[string]float64, vec2 map[string]float64) float64 {
+	all_keys := map[string]int{}
+	for key := range vec1 {
+		all_keys[key] = all_keys[key] + 1
+	}
+	for key := range vec2 {
+		all_keys[key] = all_keys[key] + 1
+	}
+
+	dot_product := 0.0
+	mag1 := 0.0
+	mag2 := 0.0
+	for key := range all_keys {
+		dot_product += vec1[key] * vec2[key]
+		mag1 += vec1[key] * vec1[key]
+		mag2 += vec2[key] * vec2[key]
+	}
+
+	mag1 = math.Sqrt(mag1)
+	mag2 = math.Sqrt(mag2)
+	if mag1 == 0 || mag2 == 0 {
+		return 0.0
+	}
+	return dot_product / (mag1 * mag2)
+}
